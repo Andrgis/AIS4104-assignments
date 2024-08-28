d72b672cfbc3f9def362c82f859fd7a967e7cfd4
andrgis <andrgis@stud.ntnu.no>
2024-08-28 19:34:28 +0200

    Implemented function functions for task 3 about transformation matrices. Added the functions transformation_matrix() and transform_vector

--- assignment-1/main.cpp
+++ assignment-1/main.cpp
@@ -22,7 +22,7 @@ Eigen::Matrix3d rotate_y(double degrees) {
     double radians = deg_to_rad(degrees);
     Eigen::Matrix3d matrix;
     matrix <<
-         std::cos(radians), 0, -std::sin(radians),
+         std::cos(radians), 0, std::sin(radians),
                          0, 1,                  0,
         -std::sin(radians), 0,  std::cos(radians);
     return matrix;
@@ -71,7 +71,6 @@ return matrix;
 Eigen::Matrix3d rotation_matrix_from_axis_angle(const Eigen::Vector3d &axis, double degrees)
 {
     Eigen::Matrix3d matrix {};
-    // implement the necessary equations and functionality.
     double radians {deg_to_rad(degrees)};
 
     Eigen::Matrix3d skewed_vector {skew_symmetric(axis)};
@@ -83,6 +82,48 @@ Eigen::Matrix3d rotation_matrix_from_axis_angle(const Eigen::Vector3d &axis, dou
     return matrix;
 }
 
+Eigen::Matrix3d rotation_matrix_from_euler_zyx(const Eigen::Vector3d &e)
+{
+    Eigen::Matrix3d I {Eigen::Matrix3d::Identity()};
+    Eigen::Matrix3d R_z {};
+    Eigen::Matrix3d R_y {};
+    Eigen::Matrix3d R_x {};
+    Eigen::Matrix3d R {};
+
+    R_z = rotate_z(e(0));
+    R_y = rotate_y(e(1));
+    R_x = rotate_x(e(2));
+
+    R = I * R_z * R_y * R_x;
+
+    return R;
+}
+
+Eigen::Matrix4d transformation_matrix(const Eigen::Matrix3d &r, const Eigen::Vector3d &p)
+{
+    Eigen::Matrix4d matrix;
+    // implement the necessary equations and functionality.
+    matrix <<
+        r(0,0), r(0,1), r(0,2), p(0),
+        r(1,0), r(1,1), r(1,2), p(1),
+        r(2,0), r(2,1), r(2,2), p(2),
+                    0,              0,            0,          1;
+    return matrix;
+}
+
+void transform_vector() {
+    Eigen::Matrix3d r {rotation_matrix_from_euler_zyx(Eigen::Vector3d{60, 45, 0})};
+    Eigen::Vector3d p {0.0, 0.0, 10.0};
+    Eigen::Vector4d v_a {2.5, 3.0, -10, 1};
+
+    Eigen::Matrix4d transform_matrix_wa {transformation_matrix(r, p)};
+
+    Eigen::Vector4d v_w{};
+    v_w = transform_matrix_wa * v_a;
+
+    std::cout << "v_w: " << std::endl << v_w << std::endl;
+
+}
 
 void example(double constant)
 {
@@ -97,8 +138,8 @@ void example(double constant)
 
 void rotation_matrix_test()
 {
-Eigen::Matrix3d rot =
-rotation_matrix_from_euler_zyx(Eigen::Vector3d{45.0, -45.0, 90.0});
+ Eigen::Matrix3d rot =
+ rotation_matrix_from_euler_zyx(Eigen::Vector3d{45.0, -45.0, 90.0});
 Eigen::Matrix3d rot_aa =
 rotation_matrix_from_axis_angle(Eigen::Vector3d{0.8164966, 0.0, 0.5773503}, 120.0);
 Eigen::Matrix3d rot_fa =
@@ -113,22 +154,20 @@ std::cout << "Rotation matrix from frame axes: " << std::endl;
 std::cout << rot_fa << std::endl << std::endl;
 }
 
+void transformation_matrix_test()
+{
+    Eigen::Matrix3d r = rotation_matrix_from_euler_zyx(Eigen::Vector3d{45, -45.0, 90.0});
+    Eigen::Vector3d v{1.0, -2.0, 3.0};
+    std::cout << "transformation_matrix: " << std::endl;
+    std::cout << transformation_matrix(r, v) << std::endl;
+}
 
 int main()
 {
-    /*example(2.0);
-    Eigen::Matrix3d x_0 = rotate_x(0);
-    Eigen::Matrix3d x_45 = rotate_x(45);
-    std::cout << x_0 << std::endl;
-    std::cout << x_45 << std::endl;
-
-    Eigen::Vector3d v(3, 5, 7);
-    Eigen::Matrix3d skew_v = skew_symmetric(v);
-    std::cout << skew_v << std::endl;*/
-
-    skew_symmetric_test(); // Test sucsesfull
-    
-
+    skew_symmetric_test(); // success
+    rotation_matrix_test(); // success
+    transformation_matrix_test(); // success
+    transform_vector(); // success?
 
     return 0;
 }
